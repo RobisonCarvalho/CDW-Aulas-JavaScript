691b2d63fa7434eaaf7479f33504a7fbb0403245
GiseleAlves <robison.carvalho12@gmail.com>
2024-06-16 23:53:02 +0100

    acrescentando aulas

---
+++ js/my_script.js
@@ -0,0 +1,41 @@
+//AULA DE CONCATENIZAÇÃO
+
+var nome = 'Gisele'
+        var idade = 30
+
+        document.write('<h1> Olã ' + nome +', tudo bem? Estou vendo que você tem '+ idade + 'anos</h1>')
+
+//FIM
+
+
+//AULA DE VARIÁVEIS
+
+var a = 10
+var b = 20
+var c = null
+
+document.write('A:' + a + '<br />')
+document.write('A:' + b + '<br />')
+document.write('A:' + c + '<br />')
+
+document.write('<hr />')
+
+// lógica de inversão dos valores das variáveis A e B 
+
+console.log(a)
+
+a = 20
+
+console.log(a)
+
+console.log(b)
+
+b = 10
+
+console.log(b)
+
+document.write('A:' + a + '<br />')
+document.write('A:' + b + '<br />')
+document.write('A:' + c + '<br />')
+
+//FIM
