d90aa3b0bac4c5ae672f2ec6888773c04fb30c94
GiseleAlves <robison.carvalho12@gmail.com>
2024-06-17 17:52:43 +0100

    Aula if e else

---
+++ js/concatenizacao.js
@@ -0,0 +1,4 @@
+var nome = 'Maria'
+var idade = 27
+
+document.write('<h2> Olã ' + nome + ', tudo bem? Estou vendo que você tem ' + idade + 'anos</h2>')
---
+++ js/ifelse.js
@@ -0,0 +1,5 @@
+if (82 <= 7) {
+	document.write ('Entrou dentro do bloco if (Verdadeiro)')
+} else {
+	document.write ('Entrou dentro do bloco else (falso)')
+}
